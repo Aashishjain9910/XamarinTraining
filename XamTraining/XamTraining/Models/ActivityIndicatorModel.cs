9adaee2935fdc7e1526882828a0dd3b0da96f892
Aashish Jain <aashishj@successive.com>
2020-05-05 21:31:38 +0530

    Activity Indicator

---
+++ XamTraining/XamTraining/Models/ActivityIndicatorModel.cs
@@ -0,0 +1,27 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamTraining.Models
+{
+    class ActivityIndicatorModel : BaseViewModel
+    {
+        private bool isLoadingData;
+
+        public bool IsLoadingData
+        {
+            get => isLoadingData;
+            set => SetProperty(ref isLoadingData, value);
+        }
+
+        public async Task LoadData()
+        {
+            IsLoadingData = true;
+            await Task.Delay(30000);
+            IsLoadingData = false;
+        }
+
+    }
+}
